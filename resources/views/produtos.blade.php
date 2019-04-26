0d8aad479efc331d9a213751d0ee99111c22ed00
Thiago Maciel <thiagomrvieira@gmail.com>
2019-04-25 22:11:23 -0300

    View Produtos - Form

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -27,7 +27,49 @@
     </div>
 </div>
 
+<div class="modal" tabindex="-1" role="dialog" id="dlgProdutos">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form class="form-horizontal" id="formProduto">
+                <div class="modal-header">
+                    <h5 class="modal-title">Novo Produto</h5>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" id="id" class="form-control">
+                    <div class="form-group">
+                        <label for="nomeProduto" class="control-label">Nome do produto</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="precoProduto" class="control-label">Preço</label>
+                        <div class="input-group">
+                            <input type="number" class="form-control" id="precoProduto" placeholder="Preço do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantidadeProduto" class="control-label">Quantidade</label>
+                        <div class="input-group">
+                            <input type="number" class="form-control" id="quantidadeProduto" placeholder="Quantidade do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="departamentoProduto" class="control-label">Departamento do produto</label>
+                        <div class="input-group">
+                            <select class="form-control" id="departamentoProduto"> </select>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <button type="cancel" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 
 
